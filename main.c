1f7e053bdb03838f2bb57ea5c43c2ae14acc7266
usoobin <112929005+usoobin@users.noreply.github.com>
2022-11-15 16:54:05 +0900

    05

--- main.c
+++ main.c
@@ -8,22 +8,21 @@
 
 int main(void)
 {
-	int i,sum,average;
-	int grade[5];
-	
-	for (i=0; i<5; i++)
-	{
-		printf("grade[%i] =", i);
-		scanf("%d",&grade[i]);
-	}
-	
-	for (i = 0; i<5; i++)
-	{
-		printf("grade[%i] = %i\n",i,*(grade+i));
-		sum = sum + *(grade+i);
-		
-	}
-	
-	printf("average = %i", sum/5);
-	
+	char *pc;
+	int *pi;
+	double *pd;
+
+	pc = (char*)10000;
+	pi = (int*)10000;
+	pd = (double*) 10000;
+
+	printf("증가 전 : pc = %d pi = %d, pd = %d\n",pc,pi,pd);
+
+	pc++;
+	pi++;
+	pd++;
+
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+
+	return 0;
 }
