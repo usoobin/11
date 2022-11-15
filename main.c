d02f482bf0fafd74cb7f088e4da3698c30cc9b8b
usoobin <112929005+usoobin@users.noreply.github.com>
2022-11-15 16:01:24 +0900

    02

--- main.c
+++ main.c
@@ -3,21 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
+//int main(int argc, char *argv[]) 
+
+void main(void)
 {
-	int i = 10;
-	char c = 'a';
+	int i = 4;
 	
-	int *iptr;
-	iptr = &i;
-	char *cptr;
-	cptr = &c;
-	int *iptr2;
-	iptr2 = iptr;
-	
-	printf("i : %p\n%p (size:%i)\n",iptr,&i,sizeof(iptr));
-	printf("c : %p\n%p (size:%i)\n",cptr,&c,sizeof(cptr));
-	printf("iptr2 : %p,%i\n",iptr,iptr2,*iptr2);
+	int *pi = &i;
+	char *pc = &i;
+
+	printf("%i, %i, %i\n", i, *pi, *pc);
+
 	
 
 	return 0;
